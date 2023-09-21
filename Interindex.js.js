44e270abb9c458956cf6980a81b9bccc51fc13da
Woosterwebdev <136399860+Woosterwebdev@users.noreply.github.com>
2023-09-20 19:13:57 -0700

    Intermediate JS notes and challenges

---
+++ Interindex.js.js
@@ -0,0 +1,68 @@
+// //Random number generator
+
+// var n = Math.random(); //generates a 16 decimal place number. Never reachs 1.0 but can be 0.
+
+// //if simulating a dice roll (six sides)
+
+// n = n * 6;
+
+// n = Math.floor(n) + 1;
+
+// console.log(n);
+
+//Love Calculator
+
+// prompt("Name #1");
+// prompt("Name #2");
+
+// var loveScore = Math.random() * 100;
+// loveScore = Math.floor(loveScore) + 1;
+// alert("Your love score is " + loveScore + "%");
+
+//Control flow
+
+// prompt("Name #1");
+// prompt("Name #2");
+
+// var loveScore = Math.random() * 100;
+// loveScore = Math.floor(loveScore) + 1;
+
+// if (loveScore > 70) {
+//     alert("Your love score is " + loveScore + "%" + " You love each other like Kanye loves Kanye.");
+// } else {
+//     alert("Your love score is " + loveScore + "%");
+// }
+
+//comparators and equality
+
+// prompt("Name #1");
+// prompt("Name #2");
+
+// var loveScore = Math.random() * 100;
+// loveScore = Math.floor(loveScore) + 1;
+
+// if (loveScore > 70) {
+//     alert("Your love score is " + loveScore + "%" + " You love each other like Kanye loves Kanye.");
+// } else {
+//     alert("Your love score is " + loveScore + "%");
+// }
+
+//comnbining comparators
+
+// prompt("Name #1");
+// prompt("Name #2");
+
+// var loveScore = Math.random() * 100;
+// loveScore = Math.floor(loveScore) + 1;
+
+// if (loveScore > 70) {
+//     alert("Your love score is " + loveScore + "%" + " You love each other like Kanye loves Kany!.");
+// } 
+
+// if (loveScore > 30 && loveScore <= 70) {
+//     alert("Your love score is " + loveScore + "%");
+// }
+    
+// if (loveScore <= 30) {
+//     alert("Your love score is " + loveScore + "%" + " You go together like oil and water.");
+// }
