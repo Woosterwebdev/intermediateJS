ae2c304b747ee22528df1a51b16bff9b2e631893
Woosterwebdev <136399860+Woosterwebdev@users.noreply.github.com>
2023-10-04 18:39:27 -0700

    Add files via upload

---
+++ DOM.js.js
@@ -0,0 +1,13 @@
+// document.getElementsByTagName("li"); //Generates all li elements from the document in an array
+// document.getElementsByTagName("li")[2].style.color = "purple"; //Would change the third li in the array to purple
+// document.getElementsByTagName("li").length; //would tell you how many li elements are in the document
+// document.getElementsByClassName("btn"); //will give you an array of elements within the document
+// document.getElementById("title") //Can only grab one as you should only use id tag once. will not return in array
+// document.querySelector("h1"); //select speciofic items from css.
+// document.querySelector(".btn");
+// document.querySelector("#title");
+// document.querySelector("li a"); //combine selectors to get specific elements. use a space to separate parent and child
+// document.querySelector("li.item"); //multiple selectors for one item do not require a space
+// document.querySelectorAll("#list .item"); //to target multiple elements
+
+// querySelector is more specific and getElement is more difficult to select specific
